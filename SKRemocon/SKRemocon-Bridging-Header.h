d474b0dded8c2c5367c0678fb06885dc0ff87e4c
kayama <kayama@kayama-pc.local>
2015-01-06 21:47:29 +0900

    init commit

---
+++ SKRemocon/SKRemocon-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  PROJECT-Bridging-Header.h
+//  PROJECT
+//
+
+
+// Write bellow setting to ""Build Settings"" -> "Swift Compiler - Code Generation"
+// $(SRCROOT)/$(PROJECT)/$(PROJECT)-Bridging-Header.h
+
+#ifndef PROJECT_Bridging_Header_h
+#define PROJECT_Bridging_Header_h
+
+// Import headder
+#include <IRKit/IRKit.h>
+
+
+#endif
---
+++ SKRemocon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SKRemocon
+//
+//  Created by kayama on 2015/01/06.
+//  Copyright (c) 2015年 kayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
